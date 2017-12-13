d872cd174d8fb108d0dfce5b9fb754000fd136b7
FJF616 <franzjfernando@hotmail.com>
2017-12-13 11:07:43 -0800

    added promise

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -3,15 +3,18 @@ import axios from 'axios';
 export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
+    let payload = {
+        
+    };
+    const apiUrl = 'http://localhost:5000/friends';
+    const friendsRequest = axios.get(apiUrl);
+    
     // Add the code for this action
-    return new Promise(function(resolve) {
-        function checkState() {
-            if (document.readyState !== 'loading') {
-                resolve();
-            }
-        }
-        document.addEventListener('readystatechange', checkState);
-        checkState();
-    });
-
+    
+    return {
+        type: GET_FRIENDS,
+        payload: friendsRequest
+    };
 };
+
+//enter redux-promise and axios
