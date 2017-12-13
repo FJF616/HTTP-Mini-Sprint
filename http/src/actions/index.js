cd24cf12339fcaa876be48b67716d466202c887b
FJF616 <franzjfernando@hotmail.com>
2017-12-13 09:29:44 -0800

    first commit

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,14 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    return new Promise(function(resolve) {
+        function checkState() {
+            if (document.readyState !== 'loading') {
+                resolve();
+            }
+        }
+        document.addEventListener('readystatechange', checkState);
+        checkState();
+    });
+
 };
